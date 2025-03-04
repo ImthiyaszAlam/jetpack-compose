a1a9a5a1a353b365eb9b67888a0e50f647f1b500
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-04 14:05:47 +0530

    launchEffect

--- ComposeFeatures/app/build.gradle.kts
+++ ComposeFeatures/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.imthiyas.composefeatures"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.imthiyas.composefeatures"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+        kotlinCompilerVersion = "1.9.0"
+    }
 }
 
 dependencies {
@@ -45,4 +54,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // Jetpack Compose libraries
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.compose.ui:ui:1.7.8")
+    implementation ("androidx.compose.material3:material3:1.3.1")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.7.8")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.7.8")
+
+    //For Compose Icons
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
--- ComposeFeatures/app/src/main/java/com/imthiyas/composefeatures/MainActivity.kt
+++ ComposeFeatures/app/src/main/java/com/imthiyas/composefeatures/MainActivity.kt
@@ -1,20 +1,70 @@
 package com.imthiyas.composefeatures
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            Counter()
         }
     }
+}
+
+
+@Composable
+fun Counter() {
+    var count = remember { mutableStateOf(0) }
+
+    val key = count.value % 3 == 0
+    LaunchedEffect(key1 = key) {
+        Log.d("COUNTONE", "${count.value}")
+    }
+
+
+    Button(onClick = { count.value++ }) {
+        Text(text = "Increment Count")
+    }
+}
+
+@Composable
+fun ListComposable() {
+    val categoryState = remember { mutableStateOf(emptyList<String>()) }
+    categoryState.value = fetchCategories()
+
+    LazyColumn(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(start = 50.dp, top = 10.dp)
+    ) {
+        items(categoryState.value) { item ->
+            Text(
+                text = item,
+                color = Color.White
+            )
+        }
+    }
+}
+
+fun fetchCategories(): List<String> {
+    return listOf("A", "B", "C")
 }
