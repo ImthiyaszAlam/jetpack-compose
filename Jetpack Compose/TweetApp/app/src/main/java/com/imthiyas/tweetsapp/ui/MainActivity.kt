3f95980f7bf2ea3143483925de9de395bbd9c687
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-16 21:58:38 +0530

    learned about navhost,navgraph and navcontroller

--- Jetpack Compose/TweetApp/app/src/main/java/com/imthiyas/tweetsapp/ui/MainActivity.kt
+++ Jetpack Compose/TweetApp/app/src/main/java/com/imthiyas/tweetsapp/ui/MainActivity.kt
@@ -3,6 +3,13 @@ package com.imthiyas.tweetsapp.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.imthiyas.tweetsapp.network.TweetsApi
 import com.imthiyas.tweetsapp.screens.CategoryScreen
 import javax.inject.Inject
@@ -16,8 +23,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CategoryScreen()
+            App()
         }
 
     }
+}
+
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "registration"
+    ) {
+        composable(route = "registration") {
+            RegistrationScreen()
+        }
+
+        composable(route = "login") {
+            LoginScreen()
+        }
+        composable(route = "main") {
+            CategoryScreen()
+        }
+    }
+}
+
+@Composable
+fun RegistrationScreen() {
+    Text(
+        text = "Registration",
+        style = MaterialTheme.typography.bodyMedium
+    )
+}
+
+@Composable
+fun LoginScreen() {
+    Text(
+        text = "Login",
+        style = MaterialTheme.typography.bodyLarge
+    )
 }
