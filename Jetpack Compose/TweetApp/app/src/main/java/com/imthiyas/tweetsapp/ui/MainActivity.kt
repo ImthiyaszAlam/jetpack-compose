fffd42f8a54b9fa72bcd2a19c1d5bfd3a5add4ec
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-19 21:18:28 +0530

    revised navcontroller,navhost ,navarguments and navgraph nodes

--- Jetpack Compose/TweetApp/app/src/main/java/com/imthiyas/tweetsapp/ui/MainActivity.kt
+++ Jetpack Compose/TweetApp/app/src/main/java/com/imthiyas/tweetsapp/ui/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavArgument
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,6 +17,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.imthiyas.tweetsapp.network.TweetsApi
 import com.imthiyas.tweetsapp.screens.CategoryScreen
+import com.imthiyas.tweetsapp.screens.TweetDetails
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -33,8 +36,30 @@ class MainActivity : AppCompatActivity() {
 }
 
 
+@Preview
 @Composable
 fun App() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "category") {
+        composable(route = "category") {
+            CategoryScreen()
+        }
+
+        composable(route = "details/{category}",
+            arguments = listOf(
+                navArgument("category")
+            ) {
+                type = NavType.StringType
+            }
+        ) {
+            TweetDetails()
+        }
+    }
+}
+
+
+@Composable
+fun App1() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
@@ -54,7 +79,7 @@ fun App() {
                 type = NavType.StringType
             }
         )) {
-           val email =  it.arguments?.getString("email")
+            val email = it.arguments?.getString("email")
             CategoryScreen(email)
         }
     }
