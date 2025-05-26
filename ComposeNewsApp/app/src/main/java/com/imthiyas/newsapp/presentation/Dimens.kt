a883eb5206f2095997df75b6077bd670db046d60
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-26 13:17:03 +0530

    title and cd

---
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/presentation/Dimens.kt
@@ -0,0 +1,9 @@
+package com.imthiyas.newsapp.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val mediumPadding1 = 24.dp
+    val mediumPadding2 = 30.dp
+
+}
--- ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/presentation/onboarding/components/OnboardingPage.kt
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/presentation/onboarding/components/OnboardingPage.kt
@@ -4,14 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.imthiyas.newsapp.R
+import com.imthiyas.newsapp.presentation.Dimens.mediumPadding1
 import com.imthiyas.newsapp.presentation.onboarding.Page
 
 
@@ -31,5 +37,24 @@ fun OnboardingPage(
         )
 
         Spacer(modifier = Modifier.height(1.dp))
+
+        page.title?.let {
+            Text(
+                text = it,
+                modifier = Modifier.padding(horizontal = mediumPadding1),
+                style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+                color = colorResource(id = R.color.black)
+            )
+        }
+
+        page.description?.let {
+            Text(
+                text = it,
+                modifier = Modifier.padding(horizontal = mediumPadding1),
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(id = R.color.white)
+            )
+
+        }
     }
 }
