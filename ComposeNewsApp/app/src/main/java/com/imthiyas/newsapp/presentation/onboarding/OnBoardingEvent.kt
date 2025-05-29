dfae6ca1b830ab7fcc40cb5575a79fb820ca1e54
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-29 16:53:08 +0530

    viewmodel scope

---
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/presentation/onboarding/OnBoardingEvent.kt
@@ -0,0 +1,5 @@
+package com.imthiyas.newsapp.presentation.onboarding
+
+sealed class OnBoardingEvent {
+    object saveAppEntry : OnBoardingViewModel()
+}
---
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/presentation/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,32 @@
+package com.imthiyas.newsapp.presentation.onboarding
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.imthiyas.newsapp.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(private val appEntryUseCases: AppEntryUseCases) : ViewModel() {
+
+
+    fun onEvent(event: OnBoardingEvent) {
+        when (event) {
+            is OnBoardingEvent.saveAppEntry -> {
+                saveAppEntry()
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+
+    private fun saveAppEntry(){
+        viewModelScope.launch {
+            appEntryUseCases.saveAppEntry()
+        }
+    }
+}
