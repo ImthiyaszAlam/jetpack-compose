a68d010518b49cc0468a36682e905d43123de0c2
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-29 16:44:02 +0530

    AndroidEntryPoint

--- ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/MainActivity.kt
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.imthiyas.newsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,16 +12,32 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.imthiyas.newsapp.domain.usecases.AppEntryUseCases
 import com.imthiyas.newsapp.presentation.onboarding.OnboardingScreen
 import com.imthiyas.newsapp.presentation.onboarding.components.OnboardingPage
 import com.imthiyas.newsapp.presentation.onboarding.pages
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var appEntryUseCases: AppEntryUseCases
+
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
+
+        lifecycleScope.launch {
+            appEntryUseCases.readAppEntry().collect {
+                Log.d("Test", it.toString())
+            }
+        }
         setContent {
             OnboardingScreen()
         }
