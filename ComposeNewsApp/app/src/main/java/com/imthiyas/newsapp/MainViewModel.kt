0b9890f20772a8f2b7a00a5306903be4a2841e56
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-31 12:57:06 +0530

    mainViewModel

--- ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/MainViewModel.kt
+++ ComposeNewsApp/app/src/main/java/com/imthiyas/newsapp/MainViewModel.kt
@@ -1,4 +1,18 @@
 package com.imthiyas.newsapp
 
-class MainViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.imthiyas.newsapp.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.reflect.Constructor
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val appEntryUseCases: AppEntryUseCases) :
+    ViewModel() {
+
+    private val _splashCondition = mutableStateOf(true)
+    val splashCondition: State<Boolean> = _splashCondition
+
 }
