216308775fcec194488bfe60548a70d21e401d95
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-30 23:58:18 +0530

    Dependencies for Compose

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
+
+    buildFeatures {
+        compose=true
+    }
     namespace = "com.mehrban.myapplication"
     compileSdk = 34
 
@@ -45,4 +50,45 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+
+    // Core
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Material
+    implementation("androidx.compose.material3:material3")
+
+    // Lifecycle + VM
+    implementation("androidx.lifecycle:lifecycle-runtime-compose")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose")
+
+    // Images
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    // Paging
+    implementation("androidx.paging:paging-compose")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // Animations
+    implementation("androidx.compose.animation:animation")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.36.0")
+    implementation("com.google.accompanist:accompanist-placeholder-material3:0.36.0")
+
+    // Optional
+    implementation("com.airbnb.android:lottie-compose:6.3.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+
 }
